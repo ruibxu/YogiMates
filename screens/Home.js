d7b0a2212a58b1554cb63952e12a6ba872d9c81d
VKwamboka <verahnyagaka101@gmail.com>
2024-02-22 22:59:56 +0100

    added foundation for home

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { theme } from '../theme';
-
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { theme } from "../theme";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 const Home = () => {
-    return (
-        <View>
-            <Text style={{ color: theme.colors.text }}>Home</Text>
-        </View>
-    );
-}
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <AntDesign name="caretleft" size={30} color="black" />
+        <Text style={{ color: theme.colors.text }}>Home</Text>
+        <FontAwesome name="circle" size={30} color="black" />
+      </View>
+    </View>
+  );
+};
 
 export default Home;
-    
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  container: {
+    padding: 15,
+  },
+});
