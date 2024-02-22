df9497e8ce6cac9836d4c8ef1042df7fee93dd80
VKwamboka <verahnyagaka101@gmail.com>
2024-02-22 23:16:51 +0100

    added foundation for home

--- screens/Home.js
+++ screens/Home.js
@@ -11,6 +11,27 @@ const Home = () => {
         <Text style={{ color: theme.colors.text }}>Home</Text>
         <FontAwesome name="circle" size={30} color="black" />
       </View>
+
+      <View style={styles.card}>
+        <View style={styles.content}>
+          <Text style={styles.title}>Daily Goals</Text>
+          <Text style={styles.description}>description</Text>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <View style={styles.content}>
+          <Text style={styles.title}>Recommendations</Text>
+          <Text style={styles.description}>description</Text>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <View style={styles.content}>
+          <Text style={styles.title}>Analysis summary</Text>
+          <Text style={styles.description}>description</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -24,5 +45,35 @@ const styles = StyleSheet.create({
   },
   container: {
     padding: 15,
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+    marginTop: 20,
+    // margin: 10,
+    padding: 10,
+    height: 200
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  content: {
+    padding: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  description: {
+    fontSize: 16,
   },
 });
