21a71d90d9f774450ad458c64520c5c3b4278d71
Ruibo Xu <ruibo.xu@stonybrook.edu>
2024-03-24 17:55:24 -0400

    analysis

---
+++ components/Graph/calories.js
@@ -0,0 +1,56 @@
+
+import { HeartRateSummary, HeartRateData, ActivitiesData} from "@dynamic-data/fitbit-data/src/mockups";
+import { View, StyleSheet,  Dimensions, ScrollView,Text } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
+import { theme } from '../../theme';
+
+const CaloriesBurnedGraph = () => {
+    // Extract calories burned data
+    const zoneNames = ActivitiesData.heartRateZones.map(zone => zone.name);
+    const caloriesBurned = ActivitiesData.heartRateZones.map(zone => zone.caloriesOut);
+  
+    // Prepare dataset
+    const dataset = {
+      labels: zoneNames,
+      datasets: [
+        {
+          data: caloriesBurned,
+          barPercentage: 0.5, // Adjust the width of the bars
+          
+        },
+      ],
+    };
+    return (
+      <View style={styles.container}>
+        <BarChart
+          data={dataset}
+          width={300}
+          height={200}
+          yAxisLabel="CB "
+          chartConfig={{
+            backgroundGradientFrom: theme.colors.background,
+            backgroundGradientTo: theme.colors.background,
+            decimalPlaces: 0,
+            color: (opacity = 1) => theme.colors.activeText, 
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      marginVertical: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
+  
+  export default CaloriesBurnedGraph;
---
+++ components/Graph/heartRate.js
@@ -0,0 +1,61 @@
+import { HeartRateSummary, HeartRateData, ActivitiesData} from "@dynamic-data/fitbit-data/src/mockups";
+import { View, StyleSheet } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
+import { theme } from '../../theme';
+const heartRate = () => {
+  const zoneNames = ActivitiesData.heartRateZones.map(zone => zone.name);
+  const maxHeartRates = ActivitiesData.heartRateZones.map(zone => zone.max);
+  const minHeartRates = ActivitiesData.heartRateZones.map(zone => zone.min);
+
+  // Prepare dataset
+  const dataset = {
+    labels: zoneNames,
+    datasets: [
+      {
+        data: maxHeartRates,
+        color: (opacity = 1) => theme.colors.activeText, // Blue color for max heart rates
+        barPercentage: 0.5, // Adjust the width of the bars
+      },
+      {
+        data: minHeartRates,
+        color: (opacity = 1) => theme.colors.activeText, // Red color for min heart rates
+        barPercentage: 0.5, // Adjust the width of the bars
+      },
+    ],
+  };
+  
+    return (
+      <View style={styles.container}>
+        <BarChart
+          data={dataset}
+          width={300}
+          height={200}
+          yAxisLabel="HR "
+          chartConfig={{
+            backgroundGradientFrom: theme.colors.background,
+            backgroundGradientTo: theme.colors.background,
+            decimalPlaces: 0,
+            color: (opacity = 1) => theme.colors.activeText, // Blue color for max heart rates
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      marginVertical: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
+
+
+export default heartRate;
